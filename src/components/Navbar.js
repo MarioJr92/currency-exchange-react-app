b275e83b16dc6e1688cba2e3227d5b40860777d6
MarioJr92 <mariorodjr92@gmail.com>
2022-10-17 05:13:16 -0400

    Navbar component complete

---
+++ src/components/Navbar.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Dropdown from "react-bootstrap/Dropdown";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-md navbar-light bg-light fixed-top">
+        <div className="d-flex flex-grow-1">
+          <span className="w-100 d-lg-none d-block"></span>
+          <a className="navbar-brand d-sm-inline-block logo" href="#">
+            Currency Exchange
+          </a>
+          <div className="w-100 text-right">
+            <Dropdown>
+              <Dropdown.Toggle variant="success" id="dropdown-basic" className="d-md-none">
+                <i className="fa fa-bars">MENU</i>
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                <Dropdown.Item href="#currencyConverter">Currency Converter</Dropdown.Item>
+                <Dropdown.Item href="#exchangeRates">Exchange Rates</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </div>
+        </div>
+        <div className="collapse navbar-collapse flex-grow-1 text-right" id="myNavbar">
+          <ul className="navbar-nav ml-auto flex-nowrap">
+            <li className="nav-item">
+              <a href="#currencyConverter" className="nav-link m-2 menu-item nav-active">
+                Currency Converter
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#exchangeRates" className="nav-link m-2 menu-item">
+                Exchange Rates
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
