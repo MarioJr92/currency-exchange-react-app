977eac912838ed877b3c1e45fafcfefb431fd3cf
MarioJr92 <mariorodjr92@gmail.com>
2022-10-17 06:58:39 -0400

    added file for CurrencyConverter comp

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import CurrencyConverter from './components/CurrencyConverter';
 
 function App() {
   return (
